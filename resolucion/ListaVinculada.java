be94eeca7ac6ca1758b38a3e0e97cf021ac4716d
bren <bhourmilougue@alumnos.exa.unicen.edu.ar>
2022-11-02 21:29:32 -0300

    clases y pensamientos

---
+++ resolucion/ListaVinculada.java
@@ -0,0 +1,21 @@
+package resolucion;
+/* Una lista vinculada ordenada es una estructura de datos formada por un conjunto de nodos, cada uno de los cuales
+* posee un objeto y una referencia al siguiente nodo de la estructura. La particularidad que tiene este tipo de listas
+* es que se encuentra siempre ordenada (ascendente o descendente según el criterio de orden con el que la lista fue creada)
+* con respecto a los elementos que contiene.
+* Implementar en Java las clases necesarias para diseñar una estructura de datos, teniendo en cuenta que el valor de cada
+* nodo puede ser cualquier tipo de objeto. NO PUEDEN USARSE COLECCIONES DE JAVA EXISTENTES PARA LA IMPLEMENTACION
+* (ArrayList, Vector, arreglos, etc). La lista vinculada debe proveer al menos a los siguientes servicios:
+* a.) Insertar un nuevo elemento en la estructura
+* b.) Eliminar un elemento de la estructura dada una posicion
+* c.) Eliminar todas las ocurrencias De un elemento de la estructura dado el elemnento
+* d.) Obtener la posicion de la primera ocurrencia de un elemento dado
+* e.) Un mecanismo que permita recorrer uno a uno los elementos
+* f.) Permitir cambiar la forma en la que se ordenan los elementos (con el subsecuente reordenamiento de los elementos
+* ya almacenados)*
+* */
+public class ListaVinculada {
+    /*PIENSO la clase lista vinculada deberia tener CAJAS VACIAS que serian los nodos que se llenarian cada una con
+    un objeto.
+    */
+}
---
+++ resolucion/Nodo.java
@@ -0,0 +1,6 @@
+package resolucion;
+/* La clase nodo deberia implementar interfaz comparable para poder ordenarse?*/
+public class Nodo {
+    private Object O;//Este es el que va a determinar qué elemento sigue, NO la estructura de la lista
+    private Nodo siguiente;//Se puede hacer esto?? Habria que buscar la forma de que este nodo pueda ser vacio
+}
