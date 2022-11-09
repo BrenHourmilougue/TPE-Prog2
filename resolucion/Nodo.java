4381aa8cd75b3ff1e1617b13071395234eec7618
bren <bhourmilougue@alumnos.exa.unicen.edu.ar>
2022-11-09 20:17:25 -0300

    AAAAAAAAAAAA

--- resolucion/Nodo.java
+++ resolucion/Nodo.java
@@ -1,6 +1,10 @@
 package resolucion;
-/* La clase nodo deberia implementar interfaz comparable para poder ordenarse?*/
+
 public class Nodo {
-    private Object objeto;//Este es el que va a determinar qué elemento sigue, NO la estructura de la lista
-    private Nodo siguiente;//Se puede hacer esto?? Habria que buscar la forma de que este nodo pueda ser vacio
+    private Nodo siguiente;
+    private Object o;
+
+    public Nodo (Object o){
+        this.o=o;
+    }
 }
---
+++ resolucion/Strategy.java
@@ -0,0 +1,4 @@
+package resolucion;
+
+public interface Strategy {
+}
