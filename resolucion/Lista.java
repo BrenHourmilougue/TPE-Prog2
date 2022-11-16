6ea07a919edc38ea881f1479b43d81fb6347c25f
bren <bhourmilougue@alumnos.exa.unicen.edu.ar>
2022-11-16 10:22:50 -0300

    fdsg

--- resolucion/Lista.java
+++ resolucion/Lista.java
@@ -13,13 +13,7 @@ public class Lista<T> /* implements Iterable<Nodo>*/{
         this.orden=orden;
         //this.size=0;
     }
-    public void mostrarLista(){
-        Iterator it = new Iterador(this.primero);
-            while (it.hasNext()){
-                Object o = it.next();
-                System.out.println(o);
-            }
-    }
+    
     public void add(T valorAInsertar){
         Nodo<T> nuevo = new Nodo<T>(valorAInsertar);
         Iterator it = new Iterador(this.primero);
@@ -28,35 +22,13 @@ public class Lista<T> /* implements Iterable<Nodo>*/{
         }
         nuevo.enlazarSiguiente((Nodo) it.next());
         ((Iterador<T>) it).getCursor().enlazarSiguiente(nuevo);
-
-        /*
-        Nodo nuevo = new Nodo(o);
-        if (primero==null){
-            primero=nuevo;
-        }
-        else if (i==0){
-
-            Nodo tmp = primero;
-            nuevo.enlazarSiguiente(tmp);
-            primero=nuevo;
-        }
-        else{
-            int cont = 0;
-            Nodo tmp = primero;
-            Nodo anterior=primero;
-            while (cont < i - 1) {
-                tmp = tmp.getSiguiente();
-                cont++;
+    }
+    public void mostrarLista(){
+        Iterator it = new Iterador(this.primero);
+            while (it.hasNext()){
+                Object o = it.next();
+                System.out.println(o);
             }
-            cont = 0;
-            while (cont<i-2){
-                anterior=anterior.getSiguiente();
-                cont++;
-            }
-            nuevo.enlazarSiguiente(tmp.getSiguiente());
-            anterior.enlazarSiguiente(nuevo);
-        }
-        size++;*/
     }
 
     public void setOrden(Comparador<T> orden) {
@@ -67,7 +39,7 @@ public class Lista<T> /* implements Iterable<Nodo>*/{
         //ordenar burbujeo o alguno por el estilo pero usando el iterator
     }
 
-    public void pop(int i){
+    public void popPosicion(int i){
         if(i==0){
             primero = primero.getSiguiente();
         }else {
@@ -77,7 +49,7 @@ public class Lista<T> /* implements Iterable<Nodo>*/{
                 it.next();
                 cont++;
             }
-            //tmp.enlazarSiguiente(tmp.getSiguiente().getSiguiente());
+            ((Iterador<T>) it).getCursor().enlazarSiguiente(((Iterador<T>) it).getCursor().getSiguiente().getSiguiente());
         }
     }
     public Object get(int i){
