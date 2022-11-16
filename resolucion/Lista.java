09b93b70d4c46403028aabec1f2583b4cc64c20b
bren <bhourmilougue@alumnos.exa.unicen.edu.ar>
2022-11-16 00:29:11 -0300

    ayudaaaa

--- resolucion/Lista.java
+++ resolucion/Lista.java
@@ -1,4 +1,18 @@
 package resolucion;
 
-public class Lista {
+public class Lista implements Recorrible{
+    @Override
+    public void irAlPrimero() {
+
+    }
+
+    @Override
+    public boolean quedanMas() {
+        return false;
+    }
+
+    @Override
+    public Object obtenerSiguiente() {
+        return null;
+    }
 }
---
+++ resolucion/Recorrible.java
@@ -0,0 +1,8 @@
+package resolucion;
+
+public interface Recorrible{
+    public void irAlPrimero();
+
+    public boolean quedanMas();
+    public Object obtenerSiguiente();
+}
